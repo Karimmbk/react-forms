02e8c2f0841f7dff997b4cb2abb8977d0dad7bc1
Karimmbk <k171395@g.unicamp.br>
2019-03-31 15:24:32 -0300

    change the file SocialContainer on the compare of the left state to change the sign-in container to the sign-up

--- src/SocialContainer.js
+++ src/SocialContainer.js
@@ -6,44 +6,30 @@ class SocialContainer extends Component {
   render() {
     const { left } = this.props;
     return (
-      <div>
-        {!left ? (
-          <div className="form-container sign-up-container">
-            <form action="#">
-              <h1>Create Account</h1>
-              <div className="social-container">
-                <a href="http://localhost:3000/" className="social">
-                  <FontAwesomeIcon icon={['fab', 'facebook-f']} />
-                </a>
-                <a href="http://localhost:3000/" className="social">
-                  <FontAwesomeIcon icon={['fab', 'google']} />
-                </a>
-                <a href="http://localhost:3000/" className="social">
-                  <FontAwesomeIcon icon={['fab', 'linkedin-in']} />
-                </a>
-              </div>
+      <div className="form-container sign-up-container">
+        <form action="#">
+          {!left ? <h1>Create Account</h1> : <h1>Sign in</h1>}
+          <div className="social-container">
+            <a href="http://localhost:3000/" className="social">
+              <FontAwesomeIcon icon={['fab', 'facebook-f']} />
+            </a>
+            <a href="http://localhost:3000/" className="social">
+              <FontAwesomeIcon icon={['fab', 'google']} />
+            </a>
+            <a href="http://localhost:3000/" className="social">
+              <FontAwesomeIcon icon={['fab', 'linkedin-in']} />
+            </a>
+          </div>
+          {!left ? (
+            <div>
               <span>or use your email for registration</span>
               <input type="text" placeholder="Name" />
               <input type="email" placeholder="Email" />
               <input type="password" placeholder="Password" />
               <button>Sign Up</button>
-            </form>
-          </div>
-        ) : (
-          <div className="form-container sign-in-container">
-            <form action="#">
-              <h1>Sign in</h1>
-              <div className="social-container">
-                <a href="http://localhost:3000/" className="social">
-                  <FontAwesomeIcon icon={['fab', 'facebook-f']} />
-                </a>
-                <a href="http://localhost:3000/" className="social">
-                  <FontAwesomeIcon icon={['fab', 'google']} />
-                </a>
-                <a href="http://localhost:3000/" className="social">
-                  <FontAwesomeIcon icon={['fab', 'linkedin-in']} />
-                </a>
-              </div>
+            </div>
+          ) : (
+            <div>
               <span>or use your account</span>
               <input type="email" placeholder="Email" />
               <input type="password" placeholder="Password" />
@@ -51,9 +37,9 @@ class SocialContainer extends Component {
                 Forgot your password?
               </a>
               <button>Sign In</button>
-            </form>
-          </div>
-        )}
+            </div>
+          )}
+        </form>
       </div>
     );
   }
